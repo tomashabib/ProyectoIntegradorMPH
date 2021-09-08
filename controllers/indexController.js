2b96af98a2fc6e8ae02747b1b4e1d75eb0c2e4aa
Pedro <ppla2001@gmail.com>
2021-09-08 15:32:07 -0300

    conecte el resto de las rutas con vistas

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,31 @@
 const controller = {
-    showIndex: function (req, res) {
-        res.render('social/index')
-    },
-    showLogin: function (req, res) {
-        res.render('social/login')
-    },
-    showRegister: function (req, res) {
-        res.render('social/registracion')
-    },
-    showAgregarPost: function (req, res) {
-        res.render('social/agregarPost')
-    },
-    showDetallePost: function (req, res) {
-        res.render('social/detallePost')
-    },
-}
+  showIndex: function (req, res) {
+    res.render("social/index");
+  },
+  showLogin: function (req, res) {
+    res.render("social/login");
+  },
+  showRegister: function (req, res) {
+    res.render("social/registracion");
+  },
+  showAgregarPost: function (req, res) {
+    res.render("social/agregarPost");
+  },
+  showDetallePost: function (req, res) {
+    res.render("social/detallePost");
+  },
+  showDetalleUsuario: function (req, res) {
+    res.render("social/detalleUsuario");
+  },
+  showEditarPerfil: function (req, res) {
+    res.render("social/editarPerfil");
+  },
+  showMiPerfil: function (req, res) {
+    res.render("social/miPerfil");
+  },
+  showResultadoBusqueda: function (req, res) {
+    res.render("social/resultadoBusqueda");
+  },
+};
 
 module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var controller = require('../controllers/indexController')
+var controller = require("../controllers/indexController");
 
 /* GET home page. */
-router.get('/', controller.showLogin);
-router.get('/index', controller.showIndex);
-router.get('/register', controller.showRegister);
-router.get('/agregarPost', controller.showAgregarPost);
-router.get('/detallePost', controller.showDetallePost);
+router.get("/", controller.showLogin);
+router.get("/index", controller.showIndex);
+router.get("/register", controller.showRegister);
+router.get("/agregarPost", controller.showAgregarPost);
+router.get("/detallePost", controller.showDetallePost);
+router.get("/detalleUsuario", controller.showDetalleUsuario);
+router.get("/editarPerfil", controller.showEditarPerfil);
+router.get("/miPerfil", controller.showMiPerfil);
+router.get("/resultadoBusqueda", controller.showResultadoBusqueda);
 
 module.exports = router;
